820c3c4beab22a42dd38a967bb9bbf1f36fe7098
Antonio Gonzalez <amgonzalezdelgado@gmail.com>
2020-11-19 21:04:02 +0100

    script test_hacker.sh done with dict inside

---
+++ dict_teams.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+declare -A repos
+
+repos[equipo_campechano]='https://github.com/rgomezh/campechano.git'
+
+repos[equipo_goodshellers]='https://github.com/ruizber23/goodshellers.git'
+
+repos[equipo_Fantasticos]='http://github.com/hxn241/Fantasticos.git'
+
+repos[equipo_Equipo_confinados]='https://github.com/danielgarciasanchez/Equipo_confinados'
+
+repos[equipo_Team5]='https://github.com/rbarlaam/Team5'
+
+repos[equipo_Los-Bashers]='https://github.com/JoaquinLou/Los-Bashers.git'
+
+echo "${!repos[@]}"
+
+for V in "${!repos[@]}"; do echo $V; done
+
