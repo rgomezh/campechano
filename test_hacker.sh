dd06872a54f32e4f47c0bec6e3ce4e510249590c
caresppen <caespe_95@hotmail.com>
2020-11-21 15:39:49 +0100

    Update test_hacker.sh

--- test_hacker.sh
+++ test_hacker.sh
@@ -1,11 +1,29 @@
 #!/bin/bash/
-# test_hacker.sh
+### test_hacker.sh
+
+# creating dictionary: teams_repos
+declare -A teams_repos
+teams_repos[goodshellers]='https://github.com/ruizber23/goodshellers.git'
+teams_repos[Fantasticos]='https://github.com/hxn241/Fantasticos.git'
+teams_repos[Equipo_confinados]='https://github.com/danielgarciasanchez/Equipo_confinados'
+teams_repos[Team5]='https://github.com/rbarlaam/Team5'
+teams_repos[Los-Bashers]='https://github.com/JoaquinLou/Los-Bashers.git'
+
+# 0. Repository of the selected team:
+echo "Team: $1"
+echo "Repository: ${teams_repos[$1]}"
 
 # 1. Create ref_repo/ folder
 mkdir ref_repo
+cd ref_repo
 
 # 2. git clone - to copy team repo to ref_repo/
+git clone ${teams_repos[$1]}
+cd goodshellers
 
+# Clean the cloned repo:
+find . -type f -not -name '*.out' -delete
+cd ..
 
 # 3. Compare *.out files - "campechano" vs "other_team"
 
