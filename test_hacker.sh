feab4a793de682715e30d9c13d90e0c775120880
caresppen <caespe_95@hotmail.com>
2020-11-21 13:41:44 +0100

    Add instructions

---
+++ test_hacker.sh
@@ -0,0 +1,27 @@
+#!/bin/bash/
+# test_hacker.sh
+
+# 1. Create ref_repo/ folder
+mkdir ref_repo
+
+# 2. git clone - to copy team repo to ref_repo/
+
+
+# 3. Compare *.out files - "campechano" vs "other_team"
+
+
+# 4. Save differences into a new folder: diff/ - "diff_[other_team].out"
+
+
+# 5. Read "diff_[other_team].out" file and show its result
+
+
+# 6. Move "[other_team]_00[i].out" -> ref_repo/hist/ & "diff_[other_team].out" -> diff/hist/
+
+
+# 7. Clean ref_repo/ & diff/
+
+
+# 8. Repeat the process for each team: for loop
+
+
