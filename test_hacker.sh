13da51a5a2c954dbac12a259072050c48038caaa
Raul Gomez <rgomez@lasprovincias.es>
2020-11-21 13:42:09 +0100

    Added test_hacker

--- test_hacker.sh
+++ test_hacker.sh
@@ -1,31 +1,34 @@
 #!/bin/bash
 
 declare -A repos
+# Create a repository associative array
+repos[campechano]='https://github.com/rgomezh/campechano.git'
+repos[goodshellers]='https://github.com/ruizber23/goodshellers.git'
+repos[Fantasticos]='http://github.com/hxn241/Fantasticos.git'
+repos[Equipo_confinados]='https://github.com/danielgarciasanchez/Equipo_confinados'
+repos[Team5]='https://github.com/rbarlaam/Team5'
+repos[Los-Bashers]='https://github.com/JoaquinLou/Los-Bashers.git'
 
-repos[equipo_campechano]='https://github.com/rgomezh/campechano.git'
+# Create a reposotitory folder if not exists and go into 
+folder="ref_repo"
+mkdir -p "$folder" && cd $_;
 
-repos[equipo_goodshellers]='https://github.com/ruizber23/goodshellers.git'
+# Clone the team repository passed by argument  
+if [ ! -d $1 ] 
+then
+	git clone -q  ${repos[$1]} 
+fi
 
-repos[equipo_Fantasticos]='http://github.com/hxn241/Fantasticos.git'
+cd ..
 
-repos[equipo_Equipo_confinados]='https://github.com/danielgarciasanchez/Equipo_confinados'
+# Get out files from the repository
+array=( $(find . -maxdepth 1 -type f -name "*.out" | sort -n) )
 
-repos[equipo_Team5]='https://github.com/rbarlaam/Team5'
-
-repos[equipo_Los-Bashers]='https://github.com/JoaquinLou/Los-Bashers.git'
-
-#echo "${!repos[@]}"
-# echo $1
-#mkdir ref_repo
-cd ref_repo
-echo ${repos[$1]}
-git clone ${repos[$1]}
-
-array=( $(find . -type f -name "*.out") )
-
-# loop over it
+# Loop over it
 for i in ${array[@]}
 do
-    echo $i
+	response="Ejercicio ${i:2} es igual"
+	(( $(diff $i $folder/$1/$i | wc -c) > 0 )) && response="Ejercicio ${i:2} NO es igual"
+	echo $response
 done
 
