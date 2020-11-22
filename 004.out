da672f108cde1128c2ee40512347dc56938da3e0
caresppen <caespe_95@hotmail.com>
2020-11-22 19:51:02 +0100

    Update test_hacker.sh

---
+++ 004.out
@@ -0,0 +1,7 @@
+     1	THIS LINE IS THE 1ST UPPER CASE LINE IN THIS FILE.
+     2	this line is the 1st lower case line in this file.
+     3	This Line Has All Its First Character Of The Word With Upper Case.
+     4	
+     5	
+     6	Two lines above this are empty.
+     7	And this is the last line.
--- test_hacker.sh
+++ test_hacker.sh
@@ -1,12 +1,13 @@
 #!/bin/bash/
 ### test_hacker.sh
+#parameters: team_name=[goodshellers, Fantasticos, Equipo_confinados, Team5, Los-Bashers]
 
 # creating dictionary: teams_repos
 declare -A teams_repos
 teams_repos[goodshellers]='https://github.com/ruizber23/goodshellers.git'
 teams_repos[Fantasticos]='https://github.com/hxn241/Fantasticos.git'
-teams_repos[Equipo_confinados]='https://github.com/danielgarciasanchez/Equipo_confinados'
-teams_repos[Team5]='https://github.com/rbarlaam/Team5'
+teams_repos[Equipo_confinados]='https://github.com/danielgarciasanchez/Equipo_confinados.git'
+teams_repos[Team5]='https://github.com/rbarlaam/Team5.git'
 teams_repos[Los-Bashers]='https://github.com/JoaquinLou/Los-Bashers.git'
 
 # 0. Repository of the selected team:
@@ -19,27 +20,42 @@ cd ref_repo
 
 # 2. git clone - to copy team repo to ref_repo/
 git clone ${teams_repos[$1]}
-cd goodshellers
+cd $1
 
 # Clean the cloned repo:
 find . -type f -not -name '*.out' -delete
 cd ..
+cd ..
 
 # 3. Compare *.out files - "campechano" vs "other_team"
+# 4. Save differences into a new folder: diffs/ - "diff_[other_team]_[00[$1]].out"
+mkdir diffs
 
+for i in {1..6}
+do
+	diff 00$i.out ref_repo/$1/00$i.out > dif.tmp # Creating a temp file w/ the output
+	cat dif.tmp > diffs/diff_$1_00$i.out
 
-# 4. Save differences into a new folder: diff/ - "diff_[other_team].out"
+	# IF output file exists and has a size greater than zero THEN the exercise was done by team
+	if [ -s ref_repo/$1/00$i.out ]
+	then
+		echo "Ejercicio $i ha sido REALIZADO por $1"
+		# IF diff file exists and has a size greater than zero THEN there is a difference
+		if [ -s diffs/diff_$1_00$i.out ]
+		then
+			echo "Ejercicio $i NO es IGUAL"
+		else
+			echo "Ejercicio $i es IGUAL"
+		fi
+	else
+		echo "Ejercicio $i NO ha sido REALIZADO por $1"
+	fi
+		
+done
 
+# 7. Clean ref_repo/
+rm dif.tmp
+rm -r ref_repo/$1 # Remove the folder of the git cloned repo
 
-# 5. Read "diff_[other_team].out" file and show its result
-
-
-# 6. Move "[other_team]_00[i].out" -> ref_repo/hist/ & "diff_[other_team].out" -> diff/hist/
-
-
-# 7. Clean ref_repo/ & diff/
-
-
-# 8. Repeat the process for each team: for loop
 
 
