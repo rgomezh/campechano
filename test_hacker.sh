f44448cca89d2c153d0f56f75f3416d63ef0aa10
Andoni Azkonobieta <aazkonobieta31@gmail.com>
2020-11-23 17:51:44 +0100

    test hacker

---
+++ test_hacker.sh
@@ -0,0 +1,10 @@
+declare -A clonequipos
+
+clonequipos[campechano]=github.com/rgomezh/campechano.git
+clonequipos[goodshellers]=github.com/ruizber23/goodshellers.git
+clonequipos[fantasticos]=github.com/hxn241/Fantasticos.git
+clonequipos[equipo_confinados]=github.com/danielgarciasanchez/Equipo_confinados
+clonequipos[team5]=github.com/rbarlaam/Team5
+clonequipos[los bashers]=github.com/JoaquinLou/Los-Bashers.git
+
+
